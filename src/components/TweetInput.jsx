4ae938a74384c79dab99e24d5066758264cf5d57
Eli Itah <eitah@cars.com>
2018-07-08 17:42:25 -0500

    WIP. Cleaned up much of logic. outstanding bug where enter still thinks you are searching

--- src/components/TweetInput.jsx
+++ src/components/TweetInput.jsx
@@ -10,48 +10,47 @@ async function getResults(search) {
     return twitterHandleSearch(search);
 }
 
+function getCurrentWord({tweet, cursorPosition}) {
+    const startOfCurrentWord = tweet.lastIndexOf(' ', cursorPosition) + 1;
+    let endOfCurrentWord = tweet.indexOf(' ', cursorPosition);
+    if (endOfCurrentWord === -1) {
+        endOfCurrentWord = tweet.length
+    }
+    const currentWord = tweet.substring(startOfCurrentWord, endOfCurrentWord);
+    return {currentWord, startOfCurrentWord};
+}
+
 class TweetInput extends Component {
     constructor() {
         super();
         this.state = {
             tweet: '',
             search: '',
-            lastAt: -1,
             userIsSearching: false,
             results: [],
+            startOfCurrentWord: 0,
         };
         this.textarea = createRef();
     }
 
     handleInput = async (e) => {
-        let {lastAt, search, results, userIsSearching} = this.state;
+        let results, search, userIsSearching;
         const tweet = e.target.value;
-        const keystroke = e.nativeEvent.data;
-
         const cursorPosition = e.target.selectionStart;
-        // confirm if user is searching
-        lastAt = tweet.lastIndexOf('@', cursorPosition);
-        // console.error('lastAt', lastAt)
-        if (lastAt !== -1) {
-            //handle if user stops searching
-            if (tweet.substr(lastAt + 1, tweet.length).split(' ').length > 1) {
-                userIsSearching = false;
-                return this.setState({userIsSearching, tweet})
-            }
 
-            // extract search from tweet
-            search = tweet.substr(lastAt + 1, tweet.length).split(' ')[0];
-            // console.error('search', search);
+        const {startOfCurrentWord, currentWord} = getCurrentWord({tweet, cursorPosition});
 
-
-            // if the search is at least 2 characters
-            if (search.length > 1) {
-                userIsSearching = true;
-                results = await getResults(search);
-            }
-            return this.setState({tweet, search, lastAt, userIsSearching, results});
+        if (currentWord.startsWith('@') && currentWord.length > 2) {
+            userIsSearching = true;
+            search = currentWord.substr(1);
+            results = await getResults(search);
+            this.setState({tweet, userIsSearching, startOfCurrentWord, search, results});
+        } else {
+            userIsSearching = false;
+            this.setState({tweet, userIsSearching, startOfCurrentWord});
         }
     };
+
 
     handleOnClick = (event, screenName) => {
         const tweet = this.replaceCurrentSearchWithCorrectHandle(screenName);
@@ -62,10 +61,10 @@ class TweetInput extends Component {
     };
 
     replaceCurrentSearchWithCorrectHandle = (resultSelected) => {
-        const {search, lastAt, tweet} = this.state;
-        const before = tweet.substring(0, lastAt + 1);
-        const after = tweet.substring(lastAt + search.length + 1, tweet.length);
-        return before + resultSelected + after;
+        const {tweet, search, startOfCurrentWord} = this.state;
+        const before = tweet.substring(0, startOfCurrentWord);
+        const after = tweet.substring(startOfCurrentWord + search.length + 1, tweet.length);
+        return before + '@' + resultSelected + after;
     };
 
     render() {
